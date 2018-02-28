9945f5edbe6a8bc93eacd6a1085fb9a6c87580a6
Adam Serbinski <adam@serbinski.com>
2018-02-28 12:29:03 -0500

    Don't build HDMI audio HAL for CAR
    HDMI and Bluetooth SCO *cannot* co-exist because they share the same
    i2s lines from the CPU.
    
    Change-Id: Icd116e22efeb1cca340d43f025b0eeb8cb26282a

--- hikey960/device-hikey960.mk
+++ hikey960/device-hikey960.mk
@@ -30,9 +30,10 @@ PRODUCT_COPY_FILES +=	$(LOCAL_PATH)/fstab.hikey960:root/fstab.hikey960 \
 PRODUCT_COPY_FILES += \
 	device/linaro/hikey/hifi/firmware/hifi-hikey960.img:system/etc/firmware/hifi/hifi.img
 
-
+ifeq ($(TARGET_PRODUCT),hikey960)
 # Build HiKey960 HDMI audio HAL. Experimental only may not work. FIXME
 PRODUCT_PACKAGES += audio.primary.hikey960
+endif
 
 PRODUCT_PACKAGES += gralloc.hikey960
 
